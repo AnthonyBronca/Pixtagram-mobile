80d57f3f570a7bdf63e2a67cc05619f24f1a5a95
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-08-09 02:40:15 -0400

    added header bar to homescreen

---
+++ components/home/Header.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image style={styles.logo} source={require('../../assets/logo.png')} />
+            </TouchableOpacity>
+
+            <View style={styles.iconsContainer}>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../assets/plus.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../assets/heart.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>14</Text>
+                    </View>
+                    <Image
+                        source={require('../../assets/message.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+    iconsContainer: {
+        flexDirection: 'row'
+    },
+    logo: {
+        width: 100,
+        height: 50,
+        resizeMode: 'contain',
+    },
+
+    icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain'
+    },
+    unreadBadge: {
+        backgroundColor: '#FF3250',
+        position: 'absolute',
+        left: 25,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100,
+    },
+
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600'
+    }
+
+})
+
+export default Header;
