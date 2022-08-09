9cc797ea616d2ced3e4a223c5c0e6bbc23968c21
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-08-09 03:04:33 -0400

    added user story place holder

---
+++ data/users.js
@@ -0,0 +1,18 @@
+export const users = [
+    {
+        user: 'AnthonyBronca',
+        image: 'https://pixtagrambucket.s3.amazonaws.com/anthony_profile_pic_seeder.png'
+    },
+    {
+        user: 'MaicaSantos',
+        image: 'https://pixtagrambucket.s3.amazonaws.com/Maica_seeder.jpg'
+    },
+    {
+        user: 'brianaR',
+        image: 'https://pixtagrambucket.s3.amazonaws.com/bri_profile_seeder.jpeg'
+    },
+    {
+        user: 'agustinZ',
+        image: 'https://pixtagrambucket.s3.amazonaws.com/agustin_seeder.jpg'
+    }
+]
