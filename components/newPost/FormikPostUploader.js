f58de3250554936aa393d3cd5423de2757f706af
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-08-11 04:18:26 -0400

    added camera roll functionality and finished upload screen

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Image, Button } from 'react-native';
 import * as Yup from 'yup'
 import { Formik } from 'formik'
-import { Divider } from 'react-native-elements';
+import { Divider, Input } from 'react-native-elements';
 import validUrl from 'valid-url'
+import * as ImagePicker from 'expo-image-picker';
 /*
 Npm install Formik
 Npm install Yup
@@ -14,13 +15,36 @@ Formik allows us to use a pre-styled form component
 */
 
 const uploadPostSchema = Yup.object().shape({
-    imageUrl: Yup.string().url().required('A URL is required'),
+    imageUrl: Yup.string().url().required('A image is required'),
     caption: Yup.string().max(2200, 'Caption has reached the character limit')
 })
 
 const FormikPostUploader = ({ navigation }) => {
 
     const [thumbnailUrl, setThumbnailUrl] = useState();
+    const [image, setImage] = useState(null);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    const handleSubmit = () => {
+        // console.log(values)
+        console.log('your post was submitted')
+        navigation.goBack()
+    }
 
     return (
         <Formik
@@ -37,14 +61,14 @@ const FormikPostUploader = ({ navigation }) => {
             {({
                 handleBlur,
                 handleChange,
-                handleSubmit,
+                // handleSubmit,
                 values,
                 errors,
                 isValid,
             }) => (
                 <>
                     <View style={{ margin: 20, justifyContent: 'space-between', flexDirection: 'row' }}>
-                        <Image style={{ width: 100, height: 100 }} source={thumbnailUrl ? { uri: validUrl.isUri(thumbnailUrl) ? thumbnailUrl : 'https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-0.jpg' } : require('../../assets/media.png')} />
+                        <Image style={{ width: 100, height: 100 }} source={image ? { uri: image ? image : 'https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-0.jpg' } : require('../../assets/media.png')} />
                         <View style={{ flex: 1, marginLeft: 12 }}>
 
                             <TextInput
@@ -59,7 +83,7 @@ const FormikPostUploader = ({ navigation }) => {
                         </View>
                     </View>
                     <Divider orientation='vertical' width={0.2} />
-                    <TextInput
+                    {/* <TextInput
                         style={{ color: 'white', fontSize: 18 }}
                         placeholder='Enter Image Url'
                         placeholderTextColor='gray'
@@ -67,12 +91,15 @@ const FormikPostUploader = ({ navigation }) => {
                         onBlur={handleBlur('imageUrl')}
                         values={values.imageUrl}
                         onChange={(e) => setThumbnailUrl(e.nativeEvent.text)}
-                    />
-                    {errors.imageUrl && (
-                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.imageUrl}</Text>
-                    )}
+                    /> */}
 
-                    <Button onPress={handleSubmit} title='Share' disabled={!isValid} />
+                    <Button title="Add Media" onPress={pickImage} />
+
+                    {/* {errors.imageUrl && (
+                        <Text style={{ fontSize: 10, color: 'red', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>{errors.imageUrl}</Text>
+                    )} */}
+
+                    <Button onPress={handleSubmit} title='Share' disabled={!image} values={values.imageUrl} />
 
                 </>
             )}
